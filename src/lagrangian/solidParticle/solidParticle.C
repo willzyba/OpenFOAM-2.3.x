bc39c4b2911e01d85af39d3222cf8e3e3544480f
Will Bateman <will@zyba.com>
2014-09-23 20:16:38 +0100

    Fixed bug in Lagrangian Particle Tracking, which caused the simulation to hang when the particle reaches the edge of the domain. Also added a switch based on the diameter of the particles, which allows the particles to have zero diameter and thus act as a tracer for use in unsteady flows

--- src/lagrangian/solidParticle/solidParticle.C
+++ src/lagrangian/solidParticle/solidParticle.C
@@ -65,38 +65,67 @@ bool Foam::solidParticle::move
         // since this will change if a face is hit
         label cellI = cell();
 
+        // trackToFace, tracks a position and returns 1.0 if the trajectory is completed
+        // without hitting a face. Otherwise it stops and returns the proportion of the trajectory
+        // that was completed.
         dt *= trackToFace(position() + dt*U_, td);
+        // TODO: Should it be possible for dt==0? Can we react smarter when this happens.
 
         tEnd -= dt;
         stepFraction() = 1.0 - tEnd/trackTime;
 
         cellPointWeight cpw(mesh_, position(), cellI, face());
-        scalar rhoc = td.rhoInterp().interpolate(cpw);
         vector Uc = td.UInterp().interpolate(cpw);
-        scalar nuc = td.nuInterp().interpolate(cpw);
 
-        scalar rhop = td.cloud().rhop();
-        scalar magUr = mag(Uc - U_);
+        // This switch allows a zero-size particle to be defined that merely acts as a tracer
+        // particle. This is usefull in combination with other flow models (ie. isoFoam) where
+        // you want to track unsteady fluid motion. 
+        if(d_>0)
+        {        
+            scalar rhoc = td.rhoInterp().interpolate(cpw);
+            scalar nuc = td.nuInterp().interpolate(cpw);
 
-        scalar ReFunc = 1.0;
-        scalar Re = magUr*d_/nuc;
+            scalar rhop = td.cloud().rhop();
+            scalar magUr = mag(Uc - U_);
 
-        if (Re > 0.01)
-        {
-            ReFunc += 0.15*pow(Re, 0.687);
+            scalar ReFunc = 1.0;
+            scalar Re = magUr*d_/nuc;
+
+            if (Re > 0.01)
+            {
+                ReFunc += 0.15*pow(Re, 0.687);
+            }
+
+            // Compute the drag coefficient. There are various formulations, 
+            // but this is 'similar' to a coefficient from "Clift et al, 1978", where 
+            //   Cd = 24 * (1+0.15 Re^0.687) / Re           
+            scalar Dc = (24.0*nuc/d_)*ReFunc*(3.0/4.0)*(rhoc/(d_*rhop));
+
+            // Compute new velocity of particle. Initial velocity + acceleration from drag
+            // This is a rearrangement to get U1, from the original equation
+            //   U1 = U0 + dt*Dc*(Uc-U1) + dt*(1-rhoc/rhop)*g
+            U_ = (U_ + dt*(Dc*Uc + (1.0 - rhoc/rhop)*td.g()))/(1.0 + dt*Dc);
         }
-
-        scalar Dc = (24.0*nuc/d_)*ReFunc*(3.0/4.0)*(rhoc/(d_*rhop));
-
-        U_ = (U_ + dt*(Dc*Uc + (1.0 - rhoc/rhop)*td.g()))/(1.0 + dt*Dc);
+        else
+        {
+            // Here we are using the particles to track the fluid flow,
+            // so the particle needs to move at whatever speed the underlying fluid moves at
+            U_ = Uc;
+        }
 
         if (onBoundary() && td.keepParticle)
         {
             if (isA<processorPolyPatch>(pbMesh[patch(face())]))
             {
+                // The particle has reached a processor boundary, so we need move it to parallel processor
                 td.switchProcessor = true;
             }
-        }
+            else
+            {
+                // The particle has reached the edge of the domain, so it must vanish
+                td.keepParticle = false;
+            }
+        } 
     }
 
     return td.keepParticle;
@@ -173,6 +202,7 @@ void Foam::solidParticle::transformProperties(const vector& separation)
 
 Foam::scalar Foam::solidParticle::wallImpactDistance(const vector&) const
 {
+    if(d_<=0) return 0.0005;  // some small number. TODO: Use a beter globally defined SMALL value. 
     return 0.5*d_;
 }
 
